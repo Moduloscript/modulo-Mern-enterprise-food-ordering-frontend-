d5cde629dd06c1d005373ea51b76d9eb969963d6
moduloscript <maduemeka254@gmail.com>
2024-05-09 21:15:13 +0100

    first commit

---
+++ src/pages/UserProfilePage.tsx
@@ -0,0 +1,22 @@
+import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
+import UserProfileForm from "@/components/forms/user-profile-form/UserProfileForm";
+
+const UserProfilePage = () => {
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
+  const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+  if (isGetLoading) {
+    return <span>Loading ....</span>;
+  }
+  if (!currentUser) {
+    return <span>Unable to load user Profile</span>;
+  }
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+    />
+  );
+};
+
+export default UserProfilePage;
