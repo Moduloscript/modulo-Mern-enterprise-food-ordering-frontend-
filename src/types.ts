7dff8148d427ea0e946e873b79dbc9a386c14f04
moduloscript <maduemeka254@gmail.com>
2024-05-21 07:22:10 +0100

    fix deploy error

--- src/types.ts
+++ src/types.ts
@@ -1,68 +1,70 @@
+import { Key } from "react";
+
 export type User = {
-    _id: string;
-    email: string;
+  _id: string;
+  email: string;
+  name: string;
+  addressLine1: string;
+  city: string;
+  country: string;
+};
+
+export type MenuItem = {
+  id: Key | null | undefined;
+  _id: string;
+  name: string;
+  price: number;
+};
+
+export type Restaurant = {
+  name: string | undefined;
+  image: string | undefined;
+  _id: string;
+  user: string;
+  restaurantName: string;
+  city: string;
+  country: string;
+  deliveryPrice: number;
+  estimatedDeliveryTime: number;
+  cuisines: string[];
+  menuItems: MenuItem[];
+  imageUrl: string;
+  lastUpdated: string;
+};
+
+export type OrderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
+
+export type Order = {
+  _id: string;
+  restaurant: Restaurant;
+  user: User;
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
     name: string;
     addressLine1: string;
     city: string;
-    country: string;
+    email: string;
   };
-  
-  export type MenuItem = {
-    id: Key | null | undefined;
-    _id: string;
-    name: string;
-    price: number;
+  totalAmount: number;
+  status: OrderStatus;
+  createdAt: string;
+  restaurantId: string;
+};
+
+export type RestaurantSearchResponse = {
+  data: Restaurant[];
+  pagination: {
+    total: number;
+    page: number;
+    pages: number;
   };
-  
-  export type Restaurant = {
-    name: string | undefined;
-    image: string | undefined;
-    _id: string;
-    user: string;
-    restaurantName: string;
-    city: string;
-    country: string;
-    deliveryPrice: number;
-    estimatedDeliveryTime: number;
-    cuisines: string[];
-    menuItems: MenuItem[];
-    imageUrl: string;
-    lastUpdated: string;
-  };
-  
-  export type OrderStatus =
-    | "placed"
-    | "paid"
-    | "inProgress"
-    | "outForDelivery"
-    | "delivered";
-  
-  export type Order = {
-    _id: string;
-    restaurant: Restaurant;
-    user: User;
-    cartItems: {
-      menuItemId: string;
-      name: string;
-      quantity: string;
-    }[];
-    deliveryDetails: {
-      name: string;
-      addressLine1: string;
-      city: string;
-      email: string;
-    };
-    totalAmount: number;
-    status: OrderStatus;
-    createdAt: string;
-    restaurantId: string;
-  };
-  
-  export type RestaurantSearchResponse = {
-    data: Restaurant[];
-    pagination: {
-      total: number;
-      page: number;
-      pages: number;
-    };
-  };
+};
