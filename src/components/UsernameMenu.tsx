4d20a76d82ec2f93a435c7a513b12a20830f4432
moduloscript <maduemeka254@gmail.com>
2024-05-29 05:52:30 +0100

    fixed an error redirect auth

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -12,20 +12,23 @@ import { Button } from "./ui/button";
 
 const UsernameMenu = () => {
   const { user, logout } = useAuth0();
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className='flex items-center px-3 font-bold hover:text-orange-500 gap-2 '>
+      <DropdownMenuTrigger className='flex items-center px-3 font-bold hover:text-orange-500 gap-2'>
         <CircleUserRound className='text-orange-500' />
         {user?.email}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-      <DropdownMenuItem>
-          <Link to='/manage-restaurant' className='font-bold hover:text-orange-400'>
+        <DropdownMenuItem>
+          <Link
+            to='/manage-restaurant'
+            className='font-bold hover:text-orange-500'>
             Manage Restaurant
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <Link to='/user-profile' className='font-bold hover:text-orange-400'>
+          <Link to='/user-profile' className='font-bold hover:text-orange-500'>
             User Profile
           </Link>
         </DropdownMenuItem>
